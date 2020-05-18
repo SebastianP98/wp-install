40c6b87e78ea9fa2bfecd60be2da93038b0df36e
Eduard Tamsa <eduard.t@skyx.com>
2020-05-18 03:27:15 +0300

    Update wp-install with new nginx config

--- wp-install.sh
+++ wp-install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+rm -r /tmp/*
 clear
 
 echo "Please provide your domain name without the www. (e.g. mydomain.com)"
@@ -9,8 +11,9 @@ apt install nginx -y
 apt install python-software-properties -y
 add-apt-repository ppa:ondrej/php -y
 add-apt-repository universe -y
+add-apt-repository ppa:certbot/certbot -y
 apt update -y
-apt install certbot php7.4-fpm php7.4-xml php7.4-mysql php7.4-dev php-mbstring php-gettext php-curl php7.4-gd php7.4-cgi -y
+apt install ed certbot python3-certbot-nginx php7.4-fpm php7.4-xml php7.4-mysql php7.4-dev php-mbstring php-gettext php-curl php7.4-gd php7.4-cgi -y
 phpenmod mbstring
 
 perl -pi -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/7.4/fpm/php.ini
@@ -18,14 +21,23 @@ perl -pi -e "s/upload_max_filesize=2M/upload_max_filesize=100M/g" /etc/php/7.4/f
 perl -pi -e "s/memory_limit = 128M/memory_limit = 256M/g" /etc/php/7.4/fpm/php.ini
 perl -pi -e "s/post_max_size = 8M/post_max_size = 100M/g" /etc/php/7.4/fpm/php.ini
 
-wget https://gist.githubusercontent.com/TaEduard/8361916fabd52e1d72b489efa3329e1c/raw/c02c6b049d51753611e8fcf01a208a679e67a4b5/nginx-wordpress
+cd /tmp
+wget https://gist.githubusercontent.com/TaEduard/8361916fabd52e1d72b489efa3329e1c/raw/9cfe94a25f523ba4cacc6def7629888825f40ac5/nginx-wordpress
 mv ./nginx-wordpress /etc/nginx/sites-available/$MY_DOMAIN
+ln -s /etc/nginx/sites-available/$MY_DOMAIN /etc/nginx/sites-enabled/
 perl -pi -e "s/example.com/$MY_DOMAIN/g" /etc/nginx/sites-available/$MY_DOMAIN
 perl -pi -e "s/www.example.com/www.$MY_DOMAIN/g" /etc/nginx/sites-available/$MY_DOMAIN
+perl -pi -e "s/#//g" /etc/nginx/sites-available/$MY_DOMAIN
 apt install mariadb-client mariadb-server -y
 apt install expect -y
 
+certbot certonly -n -d $MY_DOMAIN --agree-tos -m $EMAIL --nginx
+
+
 CURRENT_MYSQL_PASSWORD=''
+
+
+MYSQL_DOMAIN=$(echo $MY_DOMAIN| sed 's/\.//g')
 NEW_MYSQL_PASSWORD=$(openssl rand -base64 29 | tr -d "=+/" | cut -c1-25)
 SECURE_MYSQL=$(expect -c "
 set timeout 3
@@ -50,8 +62,8 @@ expect eof
 ")
 echo "${SECURE_MYSQL}"
 # Create WordPress MySQL database
-dbname="wpdbse-$MY_DOMAIN"
-dbuser="wpuser-$MY_DOMAIN"
+dbname="wpdbse_${MYSQL_DOMAIN}"
+dbuser="wpuser_${MYSQL_DOMAIN}"
 userpass=$(openssl rand -base64 29 | tr -d "=+/" | cut -c1-25)
 echo "CREATE DATABASE $dbname;" | mysql -u root -p$NEW_MYSQL_PASSWORD
 echo "CREATE USER '$dbuser'@'localhost' IDENTIFIED BY '$userpass';" | mysql -u root -p$NEW_MYSQL_PASSWORD
@@ -67,8 +79,6 @@ wget https://wordpress.org/latest.tar.gz
 mkdir /var/www/$MY_DOMAIN
 tar xzvf latest.tar.gz
 cp ./wordpress/wp-config-sample.php ./wordpress/wp-config.php
-touch ./wordpress/.htaccess
-mkdir ./wordpress/wp-content/upgrade
 cp -a ./wordpress/. /var/www/$MY_DOMAIN
 chown -R www-data /var/www/$MY_DOMAIN
 find /var/www/$MY_DOMAIN -type d -exec chmod g+s {} \;
@@ -87,8 +97,10 @@ STRING='put your unique phrase here'
 printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s /var/www/$MY_DOMAIN/wp-config.php
 
 
-# read -p "Press [ENTER] to display your WordPress MySQL database details!"
-# echo "Database Name: $dbname"
-# echo "Username: $dbuser"
-# echo "Password: $userpass"
-# echo "Your MySQL ROOT Password is: $NEW_MYSQL_PASSWORD"
+rm -r /tmp/*
+
+read -p "Press [ENTER] to display your WordPress MySQL database details!"
+echo "Database Name: $dbname"
+echo "Username: $dbuser"
+echo "Password: $userpass"
+echo "Your MySQL ROOT Password is: $NEW_MYSQL_PASSWORD"
