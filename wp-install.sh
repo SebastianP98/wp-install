3514e67d8bae6e04c8ed819f4a8004935bb80826
SebastianP98 <fluturesebastian98@gmai.com>
2020-06-01 19:35:08 +0300

    webroot

--- wp-install.sh
+++ wp-install.sh
@@ -100,7 +100,8 @@ printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s /var/www/$MY_DOMAIN/wp-config.
 apt update -y
 apt install certbot python3-certbot-nginx -y
 apt update -y
-certbot certonly -n -d $MY_DOMAIN --agree-tos -m $EMAIL --nginx
+#certbot certonly -n -d $MY_DOMAIN --agree-tos -m $EMAIL --nginx
+certbot certonly --webroot -w /var/www/$MY_DOMAIN -d $MY_DOMAIN 
 perl -pi -e "s/#//g" /etc/nginx/sites-available/$MY_DOMAIN
 service nginx restart
 
