cea67250ce0abacd591a997560712a2d2f7fbfed
SebastianP98 <fluturesebastian98@gmai.com>
2020-06-01 20:22:24 +0300

    letsencrypt

--- wp-install.sh
+++ wp-install.sh
@@ -101,7 +101,7 @@ apt update -y
 apt install certbot python3-certbot-nginx -y
 apt update -y
 #certbot certonly -n -d $MY_DOMAIN --agree-tos -m $EMAIL --nginx
-certbot certonly --webroot -w /var/www/$MY_DOMAIN/ -d $MY_DOMAIN --agree-tos -m $EMAIL
+certbot certonly --webroot -w /home/www/letsencrypt -d $MY_DOMAIN --agree-tos -m $EMAIL
 perl -pi -e "s/#//g" /etc/nginx/sites-available/$MY_DOMAIN
 service nginx restart
 
