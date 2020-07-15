8326f59ebc838f7cf2c0be4f714291f9104312f0
SebastianP98 <sebastianfluture98@gmail.com>
2020-07-15 17:54:21 +0300

    removed oms

--- wp-install.sh
+++ wp-install.sh
@@ -11,9 +11,9 @@ then
         ok=1
 fi
 
-wget https://github.com/microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.13.7-0/omsagent-1.13.7-0.universal.x64.sh
-chmod +x omsagent-1.13.7-0.universal.x64.sh
-./omsagent-1.13.7-0.universal.x64.sh --install -w 61dd12c1-c8fe-4a83-8c1f-dda7103ee57e -s o+60hlahNs58NIM19cwO4rcdeYdaXYJB2iX+DRXMV+e7i6WF2tzOzdWpDk8DWrLLy12HFIuGc31GLwJv/KUdcA==
+#wget https://github.com/microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.13.7-0/omsagent-1.13.7-0.universal.x64.sh
+#chmod +x omsagent-1.13.7-0.universal.x64.sh
+#./omsagent-1.13.7-0.universal.x64.sh --install -w 61dd12c1-c8fe-4a83-8c1f-dda7103ee57e -s o+60hlahNs58NIM19cwO4rcdeYdaXYJB2iX+DRXMV+e7i6WF2tzOzdWpDk8DWrLLy12HFIuGc31GLwJv/KUdcA==
 
 #echo "Please provide your domain name without the www. (e.g. mydomain.com)"
 #read -p "Type your domain name, then press [ENTER] : " MY_DOMAIN
