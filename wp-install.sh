42b93b9cd4549c8152300224e0c446cc345e3502
SebastianP98 <sebastianfluture98@gmail.com>
2020-07-17 18:28:35 +0300

    oms agent moved up

--- wp-install.sh
+++ wp-install.sh
@@ -41,6 +41,14 @@ else
 fi
 phpenmod mbstring
 
+update-alternatives --install /usr/sbin/python python /usr/bin/python3 20
+
+wget https://github.com/microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.13.7-0/omsagent-1.13.7-0.universal.x64.sh
+chmod +x omsagent-1.13.7-0.universal.x64.sh
+./omsagent-1.13.7-0.universal.x64.sh --install -w 61dd12c1-c8fe-4a83-8c1f-dda7103ee57e -s o+60hlahNs58NIM19cwO4rcdeYdaXYJB2iX+DRXMV+e7i6WF2tzOzdWpDk8DWrLLy12HFIuGc31GLwJv/KUdcA==
+
+
+
 perl -pi -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/7.4/fpm/php.ini
 perl -pi -e "s/upload_max_filesize=2M/upload_max_filesize=100M/g" /etc/php/7.4/fpm/php.ini
 perl -pi -e "s/memory_limit = 128M/memory_limit = 256M/g" /etc/php/7.4/fpm/php.ini
@@ -140,11 +148,11 @@ ln -s /var/log/nginx/access.log /var/log/nginx/access.txt
 ln -s /var/log/nginx/error.log /var/log/nginx/error.txt
 ln -s /var/log/syslog /var/log/syslog.txt
 
-update-alternatives --install /usr/sbin/python python /usr/bin/python3 20
+#update-alternatives --install /usr/sbin/python python /usr/bin/python3 20
 
-wget https://github.com/microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.13.7-0/omsagent-1.13.7-0.universal.x64.sh
-chmod +x omsagent-1.13.7-0.universal.x64.sh
-./omsagent-1.13.7-0.universal.x64.sh --install -w 61dd12c1-c8fe-4a83-8c1f-dda7103ee57e -s o+60hlahNs58NIM19cwO4rcdeYdaXYJB2iX+DRXMV+e7i6WF2tzOzdWpDk8DWrLLy12HFIuGc31GLwJv/KUdcA==
+#wget https://github.com/microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.13.7-0/omsagent-1.13.7-0.universal.x64.sh
+#chmod +x omsagent-1.13.7-0.universal.x64.sh
+#./omsagent-1.13.7-0.universal.x64.sh --install -w 61dd12c1-c8fe-4a83-8c1f-dda7103ee57e -s o+60hlahNs58NIM19cwO4rcdeYdaXYJB2iX+DRXMV+e7i6WF2tzOzdWpDk8DWrLLy12HFIuGc31GLwJv/KUdcA==
 
 
 read -p "Press [ENTER] to display your WordPress MySQL database details!"
