d8c2be133284ee679846447966ded113a5c12161
SebastianP98 <sebastianfluture98@gmail.com>
2020-07-23 19:44:31 +0300

    oms agent

--- wp-install.sh
+++ wp-install.sh
@@ -43,8 +43,6 @@ else
 fi
 phpenmod mbstring
 
-update-alternatives --install /usr/sbin/python python /usr/bin/python3 20
-
 #wget https://github.com/microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.13.7-0/omsagent-1.13.7-0.universal.x64.sh
 #chmod +x omsagent-1.13.7-0.universal.x64.sh
 #./omsagent-1.13.7-0.universal.x64.sh --install -w 61dd12c1-c8fe-4a83-8c1f-dda7103ee57e -s o+60hlahNs58NIM19cwO4rcdeYdaXYJB2iX+DRXMV+e7i6WF2tzOzdWpDk8DWrLLy12HFIuGc31GLwJv/KUdcA==
@@ -156,6 +154,9 @@ ln -s /var/log/syslog /var/log/syslog.txt
 #wget https://github.com/microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_v1.13.7-0/omsagent-1.13.7-0.universal.x64.sh
 #chmod +x omsagent-1.13.7-0.universal.x64.sh
 #./omsagent-1.13.7-0.universal.x64.sh --install -w 61dd12c1-c8fe-4a83-8c1f-dda7103ee57e -s o+60hlahNs58NIM19cwO4rcdeYdaXYJB2iX+DRXMV+e7i6WF2tzOzdWpDk8DWrLLy12HFIuGc31GLwJv/KUdcA==
+
+apt install python-minimal
+update-alternatives --install /usr/sbin/python python /usr/bin/python2 20
 
 wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sh onboard_agent.sh -w 61dd12c1-c8fe-4a83-8c1f-dda7103ee57e -s o+60hlahNs58NIM19cwO4rcdeYdaXYJB2iX+DRXMV+e7i6WF2tzOzdWpDk8DWrLLy12HFIuGc31GLwJv/KUdcA== -d opinsights.azure.com
 
